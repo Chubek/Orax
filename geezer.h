2cbdde635a597e8ba533dfe51c2f27dfcaa64d4d
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-24 05:52:21 +0330

    Added: declaration for AST functions

--- geezer.h
+++ geezer.h
@@ -6,16 +6,17 @@ typedef enum ASTType ASTType;
 typedef enum ASTNode ASTNode;
 
 
-ASTNode* new_ast_node(ASTType type);
-ASTNode* new_ast_leaf(ASTType type, char* value);
-ASTNode* ast_add_child(ASTNode **node, ASTNode *child);
-
-
-
-
-
-
-
+ASTNode *new_ast_node(ASTType type);
+ASTNode *new_ast_info(char *id, char *text);
+ASTNode *new_ast_cmd(char *id, ASTNode *params, ASTNode *command);
+ASTNode *new_ast_inst(char *id, ASTNode *invoke);
+ASTNode *new_ast_cmdident(char *value);
+ASTNode *new_ast_ellipse(void);
+ASTNode *new_ast_shword(char *value);
+ASTNode *new_ast_cmdtxt(char *value);
+ASTNode *new_ast_refident(char *value);
+ASTNode *ast_add_child(ASTNode **node, ASTNode *child);
+void free_ast(ASTNode *root);
 
 
 
