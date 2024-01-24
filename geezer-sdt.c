794454ff2f8191e7268c16183825a6f4e3749c93
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-24 13:49:15 +0330

    Added: walker, done with SDT for now

--- geezer-sdt.c
+++ geezer-sdt.c
@@ -192,12 +192,3 @@ void walk_and_translate(ASTNode *root, SdtCallbackFn callbackfn)
     RECURSE_TREE_LEAF();
 }
 
-
-/* Below, the various callback functions which translate the text into various languages are defnined */
-
-
-
-void sdt_to_posix_shell(int flag, char *term)
-{
-
-}
