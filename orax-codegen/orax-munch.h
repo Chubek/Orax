5791963b91189fb886982bc83d770e84f5f68f1c
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-01-31 22:57:33 +0330

    Install functions added, typedefs for term_t added and convertet necessary terminals to it -- remains passing of them to other funtions

--- orax-codegen/orax-munch.h
+++ orax-codegen/orax-munch.h
@@ -1,37 +1,40 @@
 #ifndef ORAX_MUNCH_H
 #define ORAX_MUNCH_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #include "orax-decl.h"
 
-#define NODEFN static inline MunchNode*
+#define NODEFN static inline MunchNode *
 #define LISTFN static inline MunchList
-#define MUNCHFN static inline OraxMunch*
+#define MUNCHFN static inline OraxMunch *
 #define FREEFN static inline void
 #define INSTALLFN static inline void
 #define WALKFN static inline void
 
+#define MAX_TERM 64
+
 typedef enum MunchNodeType MunchNodeType;
 typedef struct MunchNode MunchNode;
 typedef struct OraxMunch OraxMunch;
-typedef MunchNode** MunchList;
+typedef MunchNode **MunchList;
+typedef term_t term_t[static MAX_TERM];
 
 struct MunchNode {
-   MunchNodeType type;
-   MunchNode *left;
-   MunchNode *right;
-   const char *value;
-   bool is_leaf;
+  MunchNodeType type;
+  MunchNode *left;
+  MunchNode *right;
+  term_t value;
+  bool is_leaf;
 };
 
 struct OraxMunch {
   MunchList decls;
   MunchList rules;
-  const char *header;
-  const char *footer;
+  term_t header;
+  term_t footer;
   FILE *yyout;
 };
 
@@ -50,209 +53,349 @@ enum MunchNodeType {
   MUNCH_RULE_LIST,
 };
 
-
 // General factories
 
 NODEFN new_munch_node(MunchNodeType type, MunchNode *left, MunchNode *right) {
-   MunchNode *node = (MunchNode*)calloc(1, sizeof(MunchNode));
-   node->left = left;
-   node->right = right;
-   node->is_leaf = false;
-   return node;
+  MunchNode *node = (MunchNode *)calloc(1, sizeof(MunchNode));
+  node->left = left;
+  node->right = right;
+  node->is_leaf = false;
+  return node;
 }
 
-NODEFN new_munch_leaf(MunchNodeType type, const char *value) {
-   MunchNode *node = new_munch_node(type, NULL, NULL);
-   node->is_leaf = true;
-   node->value = value;
+NODEFN new_munch_leaf(MunchNodeType type, term_t value) {
+  MunchNode *node = new_munch_node(type, NULL, NULL);
+  node->is_leaf = true;
+  node->value = value;
 }
 
 // Terminal (leaf) factories
 
-NODEFN munch_ast_new_term(const char *term) {
-   return new_munch_leaf(MUNCH_TERMINAL, term);
+NODEFN munch_ast_new_term(term_t term) {
+  return new_munch_leaf(MUNCH_TERMINAL, term);
 }
 
-NODEFN munch_ast_new_non_term(const char *nonterm) {
+NODEFN munch_ast_new_non_term(term_t nonterm) {
   return new_munch_leaf(MUNCH_NON_TERMINAL, nonterm);
 }
 
-NODEFN munch_ast_new_header(const char *header) {
+NODEFN munch_ast_new_header(term_t header) {
   return new_munch_leaf(MUNCH_HEADER, header);
 }
 
-NODEFN munch_ast_new_footer(const char *footer) {
+NODEFN munch_ast_new_footer(term_t footer) {
   return new_munch_leaf(MUNCH_FOOTER, footer);
 }
 
-NODEFN munch_ast_new_semantic_action(const char *semaction) {
+NODEFN munch_ast_new_semantic_action(term_t semaction) {
   return new_munch_leaf(MUNCH_SEMANTIC_ACTION, semaction);
 }
 
-NODEFN munch_ast_new_inst(const char *instruction) {
+NODEFN munch_ast_new_inst(term_t instruction) {
   return new_munch_leaf(MUNCH_INSTRUCTION, instruction);
 }
 
 // AST factories
 
-
 NODEFN munch_ast_munch_tree(MunchNode *node1, MunchNode *node2) {
-    return new_munch_node(MUNCH_TREE, node1, node2);
+  return new_munch_node(MUNCH_TREE, node1, node2);
 }
 
 NODEFN munch_ast_munch_tree_list(MunchList trees) {
-    size_t size = 0;
-    while (trees[size] != NULL) {
-        size++;
-    }
+  size_t size = 0;
+  while (trees[size] != NULL) {
+    size++;
+  }
 
-    if (size == 1) {
-        return trees[0];
-    }
+  if (size == 1) {
+    return trees[0];
+  }
 
-    MunchNode *combined_tree = new_munch_node(MUNCH_TREE_LIST, trees[0], trees[1]);
+  MunchNode *combined_tree =
+      new_munch_node(MUNCH_TREE_LIST, trees[0], trees[1]);
 
-    for (size_t i = 2; i < size; i++) {
-        combined_tree = new_munch_node(MUNCH_TREE_LIST, combined_tree, trees[i]);
-    }
+  for (size_t i = 2; i < size; i++) {
+    combined_tree = new_munch_node(MUNCH_TREE_LIST, combined_tree, trees[i]);
+  }
 
-    return combined_tree;
+  return combined_tree;
 }
 
-NODEFN munch_ast_tree_action(MunchNode *node, const char *semantic_action) {
-    MunchNode *semActionNode = munch_ast_new_semantic_action(semantic_action);
-    return new_munch_node(MUNCH_TREE_LIST, node, semActionNode);
+NODEFN munch_ast_tree_action(MunchNode *node, term_t semantic_action) {
+  MunchNode *semActionNode = munch_ast_new_semantic_action(semantic_action);
+  return new_munch_node(MUNCH_TREE_LIST, node, semActionNode);
 }
 
-NODEFN munch_ast_rule(MunchNode *rule, const char *nonterm) {
-    MunchNode *nontermNode = munch_ast_new_non_term(nonterm);
-    return new_munch_node(MUNCH_RULE, nontermNode, rule);
+NODEFN munch_ast_rule(MunchNode *rule, term_t nonterm) {
+  MunchNode *nontermNode = munch_ast_new_non_term(nonterm);
+  return new_munch_node(MUNCH_RULE, nontermNode, rule);
 }
 
 // List factories
 
 LISTFN munch_new_list(MunchNode *init_item) {
-    MunchList list = (MunchList)calloc(2, sizeof(MunchNode*));
-    list[0] = init_item;
-    list[1] = NULL;
-    return list;
+  MunchList list = (MunchList)calloc(2, sizeof(MunchNode *));
+  list[0] = init_item;
+  list[1] = NULL;
+  return list;
 }
 
 LISTFN munch_list_append(MunchList list, MunchNode *item) {
-    size_t size = 0;
-    while (list[size] != NULL) {
-        size++;
-    }
+  size_t size = 0;
+  while (list[size] != NULL) {
+    size++;
+  }
 
-    list = (MunchList)realloc(list, (size + 2) * sizeof(MunchNode*));
-    list[size] = item;
-    list[size + 1] = NULL;
-    return list;
+  list = (MunchList)realloc(list, (size + 2) * sizeof(MunchNode *));
+  list[size] = item;
+  list[size + 1] = NULL;
+  return list;
 }
 
 // Orax munch factory
 
-MUNCHFN create_munch_state(const char *header, 
-		const char *footer,
-		MunchList decls,
-		MunchList rules) {
-    OraxMunch *munch_state = (OraxMunch*)calloc(1, sizeof(OraxMunch));
-    munch_state->header = header;
-    munch_state->footer = footer;
-    munch_state->decls = decls;
-    munch_state->rules = rules;
-    munch_state->yyout = NULL;
-    return munch_state;
+MUNCHFN create_munch_state(term_t header, term_t footer, MunchList decls,
+                           MunchList rules) {
+  OraxMunch *munch_state = (OraxMunch *)calloc(1, sizeof(OraxMunch));
+  munch_state->header = header;
+  munch_state->footer = footer;
+  munch_state->decls = decls;
+  munch_state->rules = rules;
+  munch_state->yyout = NULL;
+  return munch_state;
 }
 
 // Free functions
 
 FREEFN free_munch(MunchNode *node) {
-    if (node == NULL)
-	    return;
-    free_tree(node->left);
-    free_tree(node->right);
-    if (node->is_leaf)
-	    free(node->value);
-    FREE_AND_NULLFY(&node);
+  if (node == NULL)
+    return;
+  free_tree(node->left);
+  free_tree(node->right);
+  if (node->is_leaf)
+    free(node->value);
+  FREE_AND_NULLFY(&node);
 }
 
 FREEFN free_munch_list(MunchList list) {
-    MunchNode *node = NULL;
-    while ((node = *list++) != NULL)
-	    free_munch(node);
-    FREE_AND_NULLIFY(&list);
+  MunchNode *node = NULL;
+  while ((node = *list++) != NULL)
+    free_munch(node);
+  FREE_AND_NULLIFY(&list);
 }
 
 // C code installers
 
-INSTALLFN munch_install_macro(OraxMunch *state, 
-		const char *name, 
-		const char *definition) {
-    fprintf(state->yyout, "\n#define %s %s\n", name, definition);
+INSTALLFN munch_install_macro(OraxMunch *state, term_t name,
+                              term_t definition) {
+  fprintf(state->yyout, "#define %s %s\n", &name[0], &definition[0]);
 }
 
-INSTALLFN munch_install_word(OraxMunch *state, const char *word) {
-    fprintf(state->yyout, " %s ", word);
+INSTALLFN munch_install_include(OraxMunch *state, term_t file,
+                                term_t delim_left, term_t delim_right) {
+  fprintf(state->yyout, "#include %c%s%c\n", delim_left, file, delim_right);
 }
 
-INSTALLFN munch_install_char(OraxMunch *state, const char character) {
-    fprintf(state->yyout, " %c ", character);
+INSTALLFN munch_install_word(OraxMunch *state, term_t word) {
+  fprintf(state->yyout, " %s ", word);
 }
 
-INSTALLFN munch_install_array_literal(OraxMunch *state, 
-		const char **elements,
-		const char *term) {
-    char *element = NULL;
-    fprintf(state->yyout, "{");
-    while ((element = *elements++) != NULL)
-	    fprintf(state->yyout, "%s, ", element);
-    fprintf(state->yyout, "%s };", term);
+INSTALLFN munch_install_char(OraxMunch *state, term_t character) {
+  fprintf(state->yyout, " %c ", character);
 }
 
-INSTALLFN munch_install_array_identifier(OraxMunch *state,
-		const char *identifier,
-		const int size) {
-    fprintf(state->yyout, "%s[%d]", identifier, size);
+INSTALLFN munch_install_open_func(
+    OraxMunch *state, term_t return_type, term_t identifier,
+    struct FunctionParameter {
+      term_t type;
+      term_t ident;
+    } * *params) {
+  fprintf(state->yyout, "%s %s(", return_type, identifier);
+  struct FunctionParameter *current_param = NULL;
+
+  while ((current_param = *params++) != NULL) {
+    fprintf(state->yyout, "%s %s%c", current_param->type, current_param->ident,
+            current_param == NULL ? ')' : ',');
+
+    FREE_AND_NULLIFY(&current_param);
+  }
+
+  fprintf(state->yyout, "{\n");
 }
 
-INSTALLFN munch_install_bitfield(OraxMunch *state,
-		const char *identifier,
-		const struct BitFieldItem { 
-			const char *name;
-			int bits;
-		} **fields) {
-     fprintf(state->yyout, "struct %s {", identifier);
-     struct BitFieldItem *current_bitfield = NULL;
-     
-     while ((current_bitfield = *fields++) != NULL) {
-	fprintf("%s : %d;", 
-			current_bitfield->name, 
-			currnet_bitfield->bits);
-	FREE_AND_NULLIFY(&current_bitfield);
-     }
+INSTALLFN munch_install_close_func(OraxMunch *state) {
+  fprintf(state->yyout, "}");
+}
 
-     fprintf(state->yyout, "};");
+INSTALLFN munch_install_return(OraxMunch *state, term_t return_value) {
+  fprintf(state->yyout, "return %s;" return_value == NULL ? "" : return_value);
+}
+
+INSTALLFN munch_install_kw_sttm(OraxMunch *state, term_t keyword) {
+  fprintf(state->yyout, "%s;" keyword);
+}
+
+INSTALLFN munch_install_array_literal(OraxMunch *state, term_t *elements,
+                                      term_t term) {
+  char *element = NULL;
+  fprintf(state->yyout, "{");
+  while ((element = *elements++) != NULL) {
+    fprintf(state->yyout, "%s, ", element);
+    FREE_AND_NULLIFY(&element);
+  }
+  fprintf(state->yyout, "%s, };", term);
+}
+
+INSTALLFN munch_install_array_identifier(OraxMunch *state, term_t identifier,
+                                         const int size) {
+  fprintf(state->yyout, "%s[%d]", identifier, size);
+}
+
+INSTALLFN munch_install_bitfield(
+    OraxMunch *state, term_t identifier, const struct BitFieldItem {
+      term_t type;
+      term_t name;
+      int bits;
+    } * *fields) {
+  fprintf(state->yyout, "struct %s {\n", identifier);
+  struct BitFieldItem current_bitfield = NULL;
+
+  while ((current_bitfield = *fields++) != NULL) {
+    fprintf("%s %s : %d;\n", current_bitfield->type, current_bitfield->name,
+            currnet_bitfield->bits);
+    FREE_AND_NULLIFY(&current_bitfield);
+  }
+
+  fprintf(state->yyout, "};\n");
+}
+
+INSTALLFN munch_install_datatype(
+    OraxMunch *state, term_t identifier, term_t lexical_terminal,
+    const struct StructItem {
+      term_t type;
+      term_t name;
+    } * *items) {
+  fprintf(state->yyout, "%s %s {\n", lexical_terminal, identifier);
+  struct StructItem *current_item = NULL;
+
+  while ((current_item = *items++) != NULL) {
+    fprintf("%s %s;\n", current_item->type, current_item->name);
+    FREE_AND_NULLIFY(&current_item);
+  }
+
+  fprintf(state->yyout, "};\n");
+}
+
+INSTALLFN munch_install_typedef(OraxMunch *state, term_t main_type,
+                                term_t alias) {
+  fprintf(state->yyout, "typedef %s %s;", main_type, alias);
 }
 
 // Walker functions
 
 WALKFN walk_munch_node(MunchNode *node) {
-    if (node == NULL) {
-        return;
-    }
+  if (node == NULL) {
+    return;
+  }
 
-    if (node->is_leaf) { }
+  if (node->is_leaf) {
+  }
 
-    walk_munch_node(node->left);
-    walk_munch_node(node->right);
+  walk_munch_node(node->left);
+  walk_munch_node(node->right);
 }
 
 WALKFN walk_munch_list(MunchList list) {
-    MunchNode *node = NULL;
-    while ((node = *list++) != NULL) {
-        walk_munch_node(node);
-    }
+  MunchNode *node = NULL;
+  while ((node = *list++) != NULL) {
+    walk_munch_node(node);
+  }
 }
+
+// Renaming Lex and Yacc identifiers so they won't collide with other instances
+// of Lex and Yacc in Orax project
+
+#define yyinput munch_yylex
+#define yyoutput munch_yyout
+#define yyunput munchyyunput
+#define yywrap munch_yywrap
+
+#define yylex munch_yylex
+#define yyerror munch_yyerror
+#define yychar munch_yychar
+#define yylval munch_yylval
+#define yydebug munch_yydebug
+#define yyec munch_yyec
+
+#define yyparse munch_yyparse
+#define yyparserr munch_yyparserr
+#define yydebug munch_yydebug
+#define yyval munch_yyval
+#define yyss munch_yyss
+#define yyssp munch_yyssp
+#define yystate munch_yystate
+#define yytmp munch_yytmp
+#define yyv munch_yyv
+#define yyvsp munch_yyvsp
+#define yylook munch_yylook
+#define yyback munch_yyback
+#define yyreduce munch_yyreduce
+#define yydefact munch_yydefact
+#define yydefgoto munch_yydefgoto
+#define yypact munch_yypact
+#define yypgoto munch_yypgoto
+#define yytable munch_yytable
+#define yycheck munch_yycheck
+#define yygbase munch_yygbase
+#define yygindex munch_yygindex
+#define yylen munch_yylen
+#define yydefred munch_yydefred
+#define yydgoto munch_yydgoto
+#define yysindex munch_yysindex
+#define yyrindex munch_yyrindex
+#define yyss munch_yyss
+#define yyssp munch_yyssp
+#define yystate munch_yystate
+#define yytmp munch_yytmp
+#define yyv munch_yyv
+#define yyvsp munch_yyvsp
+#define yylook munch_yylook
+#define yyback munch_yyback
+#define yyreduce munch_yyreduce
+#define yydefact munch_yydefact
+#define yydefgoto munch_yydefgoto
+#define yypact munch_yypact
+#define yypgoto munch_yypgoto
+#define yytable munch_yytable
+#define yycheck munch_yycheck
+#define yygbase munch_yygbase
+#define yygindex munch_yygindex
+#define yylen munch_yylen
+#define yydefred munch_yydefred
+#define yydgoto munch_yydgoto
+#define yysindex munch_yysindex
+#define yyrindex munch_yyrindex
+#define yyss munch_yyss
+#define yyssp munch_yyssp
+#define yystate munch_yystate
+#define yytmp munch_yytmp
+#define yyv munch_yyv
+#define yyvsp munch_yyvsp
+#define yylook munch_yylook
+#define yyback munch_yyback
+#define yyreduce munch_yyreduce
+#define yydefact munch_yydefact
+#define yydefgoto munch_yydefgoto
+#define yypact munch_yypact
+#define yypgoto munch_yypgoto
+#define yytable munch_yytable
+#define yycheck munch_yycheck
+#define yygbase munch_yygbase
+#define yygindex munch_yygindex
+#define yylen munch_yylen
+#define yydefred munch_yydefred
+#define yydgoto munch_yydgoto
+#define yysindex munch_yysindex
+#define yyrindex munch_yyrindex
 
 #endif
