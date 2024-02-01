24cc2d12cc19ef0363a43981c9724aa38feeffba
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-02-01 07:18:36 +0330

    Added: the variable/constant distinction

--- orax-inst.c
+++ orax-inst.c
@@ -5,20 +5,21 @@
 #include <string.h>
 
 #include "orax-decl.h"
+#include "orax-enums.h"
 
 struct Instruction {
   instid_t id;
   InstructionClass class;
-  InstructionName type;
+  InstructionName name;
   Operand **operands;
   size_t num_operands;
   Result *result;
 };
 
-struct Operand {
-  ophash_t hash;
+struct Variable {
+  varhash_t hash;
   ssaversion_t ssa_version;
-  OperandType type;
+  VariableType type;
   typsize_t size;
   union {
     unsigned_integral_t unsigned_integeral;
@@ -29,63 +30,114 @@ struct Operand {
   };
 };
 
+struct Constant {
+    ConstantType type;
+    union {
+	unsigned_integral_t unsigned_integral;
+	signed_integral_t signed_integral;
+	rational_t rational;
+	boolean_t boolean;
+    };
+};
+
+struct Operand {
+   OperandType type;
+   union {
+	Variable *variable;
+	Constant *constant;
+   };
+};
+
 Instruction *create_instruction(InstructionName type, InstructionClass class,
                                 instid_t id) {
   Instruction *inst = (Instruction *)calloc(1, sizeof(Instruction));
   inst->id = id;
   inst->class = class;
   inst->type = type;
-  inst->operands = NULL;
-  inst->num_operands = 0;
+  inst->variables = NULL;
+  inst->num_variables = 0;
   inst->result = NULL;
   return inst;
 }
 
-Operand *create_operand(ophash_t hash, OperandType type, void *value,
+Variable *create_variable(varhash_t hash, VariableType type, void *value,
                         typesize_t size) {
-  Operand *operand = (Operand *)calloc(1, sizeof(Operand));
-  operand->hash = hash;
-  operand->size = size;
-  operand->type = type;
-  operand->ssa_version = SSA_VERSION_UNASSIGNED;
+  Variable *variable = (Variable *)calloc(1, sizeof(Variable));
+  variable->hash = hash;
+  variable->size = size;
+  variable->type = type;
+  variable->ssa_version = SSA_VERSION_UNASSIGNED;
 
   switch (type) {
   case OPTYPE_UNSIGNED_INTEGRAL:
-    operand->unsigned_integeral = *((unsigned_integral_t *)value);
+    variable->unsigned_integeral = *((unsigned_integral_t *)value);
     break;
   case OPTYPE_SIGNED_INTEGRAL:
-    operand->signed_integral = *((signed_integral_t *)value);
+    variable->signed_integral = *((signed_integral_t *)value);
     break;
   case OPTYPE_RATIONAL:
-    operand->rational = *((rational_t *)value);
+    variable->rational = *((rational_t *)value);
     break;
   case OPTYPE_MEMORY_POINTER:
-    operand->memory_pointer = value;
+    variable->memory_pointer = value;
     break;
   case OPTYPE_BOOLEAN:
-    operand->boolean = *((boolean_t *)value);
+    variable->boolean = *((boolean_t *)value);
     break;
   default:
     break;
   }
 
-  return operand;
+  return variable;
 }
 
-void add_to_operand_size(Operand *op, typesize_t addition) {
+Constant *create_constant(ConstantType type, void *value) {
+  Constant *cons = (Constant*)calloc(1, sizeof(Constant));
+  cons->type = type;
+  
+  switch (type) {
+  case CONTYPE_UNSIGNED_INTEGRAL:
+	  cons->unsigned_integer = *((unsigned_integral_t *)value);
+	  break;
+  case CONTYPE_SIGNED_INTEGRAL:
+	  cons->signed_integer = *((signed_integral_t *)value);
+	  break;
+  case CONTYPE_RATIONAL:
+	 cons->rational = *((rational_t *)value);
+	  break;
+  case CONTYPE_BOOLEAN:
+	  cons->boolean = *((boolean_t *)value);
+	  break;
+  }
+  
+  return cons;
+}
+
+
+Operand *create_operand(OperandType type, void *value) {
+  Opernad *op = (Opernad*)calloc(1, sizeof(Operand));
+
+  if (type == CONTYPE_CONSTANT)
+	  op->constant = (Constant*)value;
+  else
+	  op->variable = (Variable*)value;
+}
+
+
+void add_to_variable_size(Variable *op, typesize_t addition) {
   op->size += addition;
 }
 
-void subtract_from_operand_size(Operand *op, typesize_t subtraction) {
+void subtract_from_variable_size(Variable *op, typesize_t subtraction) {
   if (op->size - subtraction = < 0)
     return;
   op->size -= subtraction;
 }
 
-Instruction *add_inst_operand(Instrution *inst, Operand *operand) {
-  inst->operands = (Operand **)realloc(inst->operands, (inst->num_operand + 1) *
-                                                           sizeof(Operand *));
-  inst->operands[inst->num_operands++] = operand;
+Instruction *add_inst_variable(Instrution *inst, Variable *variable) {
+  inst->variables = (Variable **)realloc(inst->variables, (inst->num_variable + 1) *
+                                                           sizeof(Variable *));
+  inst->variables[inst->num_variables++] = variable;
   return inst;
 }
 
@@ -94,54 +146,60 @@ Instruction *add_inst_result(Instruction *inst, Result *result) {
   return inst;
 }
 
-Operand *duplicate_operand(Operand *op) {
-  Operand *operand_copy = (Operand *)calloc(1, sizeof(Operand));
-  operand_copy = memmove(operand_copy, op, sizeof(Operand));
-  operand_copy->id = GEN_UNIQUE_ID();
-  return operand_copy;
+Variable *duplicate_variable(Variable *op) {
+  Variable *variable_copy = (Variable *)calloc(1, sizeof(Variable));
+  variable_copy = memmove(variable_copy, op, sizeof(Variable));
+  variable_copy->id = GEN_UNIQUE_ID();
+  return variable_copy;
 }
 
-bool operand_pair_is_signed_integral(Operand *op1, Operand *op2) {
-  return op1->type == OPTYPE_SIGNED_INTEGERAL
-	  	&& op2->type == OPTYPE_SIGNED_INTEGERAL;
+Constant *duplicate_constant(Constant *cons) {
+  Constnat *constant_copy = (Constant *)calloc(1, sizeof(Constant));
+  constant_copy = memmove(constant_copy, cons, sizeof(Constant));
+  return constant_copy;
 }
 
-bool operand_pair_is_unsigned_integral(Operand *op1, Operand *op2) {
-  return op1->type == OPTYPE_UNSIGNED_INTEGERAL
-	  	&& op2->type == OPTYPE_UNSIGNED_INTEGERAL;
+bool constant_pair_is_signed_integral(Constant *cons1, Constant *cons2) {
+  return cons1->type == CONTYPE_SIGNED_INTEGERAL
+	  	&& cons2->type == CONTYPE_SIGNED_INTEGERAL;
 }
 
-bool operand_pair_is_rational(Operand *op1, Operand *op2) {
-  return op1->type == OPTYPE_RATIONAL
-	  	&& op2->type == OPTYPE_RATIONAL;
+bool constant_pair_is_unsigned_integral(Constant *cons1, Constant *cons2) {
+  return cons1->type == CONTYPE_UNSIGNED_INTEGERAL
+	  	&& cons2->type == CONTYPE_UNSIGNED_INTEGERAL;
 }
 
-bool operand_pair_is_memory_pointer(Operand *op1, Operand *op2) {
-  return op1->type == OPTYPE_MEMORY_POINTER
-	  	&& op2->type == OPTYPE_MEMORY_POINTER;
+bool constant_pair_is_rational(Constant *cons1, Constant *cons2) {
+  return cons1->type == CONTYPE_RATIONAL
+	  	&& cons2->type == CONTYPE_RATIONAL;
+}
+
+bool constant_pair_is_boolean(Constant *cons1, Constant *cons2) {
+  return cons1->type == CONTYPE_BOOLEAN
+	  	&& cons2->type == CONTYPE_BOOLEAN;
 }
 
 Result *create_result(ResultType type, void *value) {
-  return (Result *)create_operand(type, value);
+  return (Result *)create_variable(type, value);
 }
 
-void free_operand(Operand *op) {
+void free_variable(Variable *op) {
   if (op == NULL)
     return;
 
-  if (op->type == OPTYPE_PTR && op->ptr != NULL)
+  if (op->type == CONTYPE_PTR && op->ptr != NULL)
     FREE_AND_NULLIFY(&op->ptr);
-  else if (op->type == OPTYPE_STR && op->str != NULL)
+  else if (op->type == CONTYPE_STR && op->str != NULL)
     FREE_AND_NULLIFY(&op->str);
 
   FREE_AND_NULLIFY(&op);
 }
 
 void free_instruction(Instruction *inst) {
-  while (--inst->num_operands)
-    free_operand(inst->operands[inst->num_operands]);
+  while (--inst->num_variables)
+    free_variable(inst->variables[inst->num_variables]);
 
-  free_operand(inst->result);
+  free_variable(inst->result);
 
   FREE_AND_NULLIFY(&inst);
 }
